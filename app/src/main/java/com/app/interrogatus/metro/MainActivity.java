f8c077838ecda5b86aee2ef87d04f58eecc27c7c
Oleg Dubrovin <interrogatus@gmail.com>
2017-03-08 11:52:08 +0300

    Появилось древо линий

--- app/src/main/java/com/app/interrogatus/metro/MainActivity.java
+++ app/src/main/java/com/app/interrogatus/metro/MainActivity.java
@@ -2,37 +2,35 @@ package com.app.interrogatus.metro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView recycler;
 
     List<String> stations = new ArrayList<>();
+    HashMap<String, List<String>> treeStations = new HashMap<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        stations.add(0, "Полянка");
-        stations.add(1, "Автозаводская");
-        stations.add(2, "Боровицкая");
-        stations.add(3, "Полянка");
-        stations.add(4, "Автозаводская");
-        stations.add(5, "Боровицкая");
-        stations.add(6, "Полянка");
-        stations.add(7, "Автозаводская");
-        stations.add(8, "Боровицкая");
+        fillTreeCollection();
+        fillAdapterCollection(true);
 
         recycler = (RecyclerView) findViewById(R.id.recycler);
 
@@ -41,11 +39,54 @@ public class MainActivity extends AppCompatActivity {
 
         RecyclerAdapter adapter = new RecyclerAdapter();
         recycler.setAdapter(adapter);
-
+        recycler.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
     }
 
-   class  RecyclerAdapter extends RecyclerView.Adapter<StationHolder>{
+    private void fillAdapterCollection(boolean onLinies) {
+        stations.clear();
+
+        int count=0;
+        for (Map.Entry entry:treeStations.entrySet()){
+            List<String> value = (List)entry.getValue();
+
+                if(onLinies==true){
+                    stations.add(count,(String)entry.getKey());
+                    count++;
+                }
+                for (String i:value){
+                    stations.add(count,i);
+                    count++;
+                }
+        }
+    }
+
+    //1.Заполняем древо станций
+    private void fillTreeCollection() {
+
+        List<String> a = Arrays.asList(new String[] {"a", "b", "c"});;
+        treeStations.put("Сокольническая", Arrays.asList(
+                new String[] {
+                        "Кропоткинская",
+                        "Красные Ворота",
+                        "Чистые пруды"
+                }));
+        treeStations.put("Замоскворецкая", Arrays.asList(
+                new String[] {
+                        "Автозаводская",
+                        "Царицыно",
+                        "Домодедовская"
+                }));
+        treeStations.put("Арбатско-Покровская", Arrays.asList(
+                new String[] {
+                        "Арбатская",
+                        "Партизанская",
+                        "Семеновская"
+                }));
+
+    }
+
+    class  RecyclerAdapter extends RecyclerView.Adapter<StationHolder>{
 
        @Override
        public StationHolder onCreateViewHolder(ViewGroup parent, int viewType) {
