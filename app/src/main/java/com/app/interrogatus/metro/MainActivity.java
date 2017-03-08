a897e799318fc9b54370d55ed75f3470fef03f48
Oleg Dubrovin <interrogatus@gmail.com>
2017-03-08 13:48:57 +0300

    Вывод станций в отдельном лэйауте

--- app/src/main/java/com/app/interrogatus/metro/MainActivity.java
+++ app/src/main/java/com/app/interrogatus/metro/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -22,6 +24,8 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> stations = new ArrayList<>();
     HashMap<String, List<String>> treeStations = new HashMap<>();
+    RecyclerAdapter adapter = new RecyclerAdapter();
+    boolean sortOnLinies = false;
 
 
     @Override
@@ -30,27 +34,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         fillTreeCollection();
-        fillAdapterCollection(true);
+        fillAdapterCollection();
 
         recycler = (RecyclerView) findViewById(R.id.recycler);
 
         LinearLayoutManager llm = new LinearLayoutManager(this);
         recycler.setLayoutManager(llm);
 
-        RecyclerAdapter adapter = new RecyclerAdapter();
         recycler.setAdapter(adapter);
         recycler.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
 
     }
 
-    private void fillAdapterCollection(boolean onLinies) {
+    private void fillAdapterCollection() {
         stations.clear();
 
         int count=0;
         for (Map.Entry entry:treeStations.entrySet()){
             List<String> value = (List)entry.getValue();
 
-                if(onLinies==true){
+                if(sortOnLinies==true){
                     stations.add(count,(String)entry.getKey());
                     count++;
                 }
@@ -64,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
     //1.Заполняем древо станций
     private void fillTreeCollection() {
 
-        List<String> a = Arrays.asList(new String[] {"a", "b", "c"});;
         treeStations.put("Сокольническая", Arrays.asList(
                 new String[] {
                         "Кропоткинская",
@@ -91,7 +93,17 @@ public class MainActivity extends AppCompatActivity {
        @Override
        public StationHolder onCreateViewHolder(ViewGroup parent, int viewType) {
            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-           View view = inflater.inflate(R.layout.listitem, parent, false);
+
+           View view = null;
+
+           switch (viewType){
+               case 1:
+                    view = inflater.inflate(R.layout.listitemgroup, parent, false);
+                    break;
+               case 0:
+                    view = inflater.inflate(R.layout.listitem, parent, false);
+                    break;
+           }
            StationHolder holder = new StationHolder(view);
            return holder;
        }
@@ -109,7 +121,16 @@ public class MainActivity extends AppCompatActivity {
            return stations.size();
        }
 
-   }
+        @Override
+        public int getItemViewType(int position) {
+            if (treeStations.containsKey(stations.get(position))==true)
+            {
+                return  1;
+            }
+            else return 0;
+
+        }
+    }
     class StationHolder extends RecyclerView.ViewHolder{
 
         TextView namestation;
@@ -120,5 +141,23 @@ public class MainActivity extends AppCompatActivity {
             namestation = (TextView) itemView.findViewById(R.id.namestation);
 
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.sort){
+            sortOnLinies=!sortOnLinies;
+
+            fillAdapterCollection();
+
+            adapter.notifyDataSetChanged();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
